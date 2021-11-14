32320a58fa77e8385cf4db4cfadd942a2090a7d0
Unlikeghost <unlikeghost@protonmail.com>
2021-11-14 13:26:49 -0700

    rutas

--- app/errors/routes.py
+++ app/errors/routes.py
@@ -2,7 +2,6 @@ from flask.helpers import flash
 from . import errors_bp
 from flask import url_for
 from flask import redirect
-from flask import render_template
 
 @errors_bp.app_errorhandler(401)
 def error_401(error):
@@ -10,9 +9,9 @@ def error_401(error):
     return redirect(url_for('public.index'))
 
 
-@errors_bp.errorhandler(404)
-def resource_not_found(e):
+@errors_bp.app_errorhandler(404)
+def error_404(e):
 
     flash("Tenemos errores en este momento", "error")
 
-    return redirect(url_for('public.index'), code=404)
+    return redirect(url_for('public.index'))
